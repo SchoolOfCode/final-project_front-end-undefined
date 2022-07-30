787a1f5486a2204c66095608ddc9228f09d6e40a
enantes <sambaeverything@gmail.com>
2022-07-30 14:08:03 +0200

    new

--- frontend/components/List.jsx
+++ frontend/components/List.jsx
@@ -1,6 +1,6 @@
+import { Box, Flex, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
 import React from "react";
 import PlaceDetail from "./PlaceDetail";
-import { Flex, Box, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
 
 const List = ({ places, isLoading }) => {
   if (isLoading)
@@ -9,15 +9,19 @@ const List = ({ places, isLoading }) => {
         direction={"column"}
         bg={"whiteAlpha.900"}
         width={"37vw"}
-        height="100vh" //may need {} around the ""
+        height="100vh"
         position={"absolute"}
         left={0}
         top={0}
         zIndex={1}
-        overflow="hidden" //may need {} around the ""
+        overflow="hidden"
         px={2}
       >
         <Box padding="6" boxShadow="lg" bg="white" mt={16}>
+          <SkeletonCircle size="10" />
+          <SkeletonText mt="4" noOfLines={4} spacing="4" />
+        </Box>
+        <Box padding="6" boxShadow="lg" bg="white" mt={3}>
           <SkeletonCircle size="10" />
           <SkeletonText mt="4" noOfLines={4} spacing="4" />
         </Box>
@@ -41,12 +45,12 @@ const List = ({ places, isLoading }) => {
       direction={"column"}
       bg={"whiteAlpha.900"}
       width={"37vw"}
-      height="100vh" //may need {} around the ""
+      height="100vh"
       position={"absolute"}
       left={0}
       top={0}
       zIndex={1}
-      overflow="hidden" //may need {} around the ""
+      overflow="hidden"
       px={2}
     >
       <Flex flex={1} overflowY={"scroll"} mt={16} direction={"column"}>
