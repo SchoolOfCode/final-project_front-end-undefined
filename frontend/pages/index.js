e573310514df39cefcd6410dc6ec2ce983986fe9
enantes <sambaeverything@gmail.com>
2022-07-30 13:41:46 +0200

    map component

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,33 +1,48 @@
 import { Flex } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import Header from "../components/Header";
 import List from "../components/List";
 import Map from "../components/Map";
 import PlaceDetail from "../components/PlaceDetail";
-import { useEffect, useState } from "react";
+import { getPlacesData } from "./api";
+import Head from "next/head";
 
-const places = [
-  { name: "sample Place1" },
-  { name: "sample Place1" },
-  { name: "sample Place1" },
-  { name: "sample Place1" },
-];
+const GOOGLE_API = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY
 
 const Home = () => {
-  //The 3 states below are for our Header component
-  const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
+  const [places, setPlaces] = useState([]);
+  const [filteredPlaces, setFilteredPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState(null);
   const [type, setType] = useState("restaurants");
   const [ratings, setRatings] = useState("");
-  //This state is for talking to the API
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    // get the users current location on intial login 
+    // get the users current location on intial login
 
-    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
-    console.log({latitude, longitude});
-    })
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        console.log({ latitude, longitude });
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
 
-  }, [])
+  useEffect(() => {
+    const filteredData = places.filter((place) => place.rating > ratings);
+    setFilteredPlaces(filteredData);
+    console.log({ ratings });
+  }, [ratings]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    getPlacesData(type, bounds?.sw, bounds?.ne).then((data) => {
+      console.log(data);
+      setPlaces(data);
+      setIsLoading(false);
+    });
+  }, [type, coordinates, bounds]);
 
   return (
     <Flex
@@ -39,15 +54,27 @@ const Home = () => {
       maxHeight={"100vh"}
       position={"relative"}
     >
+      <Head>
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyD93tjfea30qHGkuhHJWQ0vQB9FF-HYIZo"></script>
+      </Head>
+
       <Header
         setType={setType}
         setRatings={setRatings}
         setCoordinates={setCoordinates}
       />
 
-      <List places={places} isLoading={isLoading} />
+      <List
+        places={filteredPlaces.length ? filteredPlaces : places}
+        isLoading={isLoading}
+      />
 
-      <Map setCoordinates={setCoordinates} coordinates={coordinates} />
+      <Map
+        setCoordinates={setCoordinates}
+        coordinates={coordinates}
+        setBounds={setBounds}
+        places={filteredPlaces.length ? filteredPlaces : places}
+      />
     </Flex>
   );
 };
