6762d629fa503c51029b0d6ffc6d17c9732a6fb2
Phoebe Windham <phoebe.frances@outlook.com>
2022-08-08 21:15:29 +0100

    added comments to the code and isLoading and error to profile page

--- frontend/pages/profile.js
+++ frontend/pages/profile.js
@@ -6,9 +6,17 @@ export default function Profile() {
 
     const {user, error, isLoading} = useUser();
 
+    // Ensures  that the SDK has completed loading before accessing the user property by checking that isLoading is false.
+    if (isLoading) return <div>Loading...</div>;
+
+    // Ensures that the SDK has loaded successfully by checking that no error was produced.
+    if (error) return <div>{error.message}</div>;
+
+    // Checks the user property to ensure that Auth0 has authenticated the user before React renders any component that consumes it.
     if (user) {
         return (
             <div>
+            <img src={user.picture} alt={user.name} />
             <h1>Welcome {user.name}</h1>
             <a href="/api/auth/logout">Logout</a>
             </div>
