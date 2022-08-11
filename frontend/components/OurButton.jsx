0f8b1078fe5fa2db46d56060698ded4ffe59cd7e
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-08-11 20:23:30 +0100

    added _hover to OurButton

--- frontend/components/OurButton.jsx
+++ frontend/components/OurButton.jsx
@@ -4,6 +4,7 @@ const OurButton = ({ text, icon, onClick }) => {
   return (
     <Button
       bgGradient="linear(to-r, #17CEDA, #032396)"
+      _hover={{backgroundImage: "linear-gradient(to right, #19f7fa , #1c8cfb)"}}
       rounded="full"
       leftIcon={icon}
       color="white"
